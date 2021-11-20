06b3c3e121177afa658d29462644782484402ecc
meenank <varunmeenank19@gmail.com>
2021-11-20 16:41:24 +0530

    added css styling and post functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 //render css files
